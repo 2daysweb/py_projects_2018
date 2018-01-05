7c5a05b05c4610cacb1197737bdef106e03fbf77
Sahnun <shmohamud@email.wm.edu>
2018-01-05 16:09:58 -0500

    fizz_buzz_pysyntax
    Print numbers in range 1-100,
    If multiples of 3 print "fizz"
    Elif multiple of 5 print "buzz"
    Elif multiple of 3 AND 5 print "fizzbuzz"

---
+++ fizzbuzzyy.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 16:11:06 2018
+
+@author: Sahnun
+"""
+
+for i in range(1, 101):
+    if (i%3 == 0 and i%5 == 0):
+        print("fizzbuzz")
+    elif i%5 == 0:
+        print("buzz")
+    if i%3 == 0: 
+        print("fizz")
+    else:
+        print(i)
+   
+  #WORKING!
+  
+  
+  
+    
+   
+    
+    
+    
+
+
+    
+    
+
+
+
+
+    
+    
+        
+        
+    
+
